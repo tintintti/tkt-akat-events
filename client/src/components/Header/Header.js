9c1cae4e8c85ed43d5e5260d244414496f9f7fe3
Tintti Rahikainen <tintti.rahikainen@gmail.com>
2017-06-27 22:06:28 +0300

    redesigned to look little less crappy

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -29,6 +29,7 @@ class Header extends Component {
     render() {
         return (
             <div className="Header">
+                <span id="akat">TKT-AKAT</span><br/>
             <nav>
                     {this.props.isAuthenticated ?
                     <ul>
